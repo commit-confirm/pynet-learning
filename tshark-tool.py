5f98db61091875034f2c13e4998a68ff876a2620
Lee Dowdells <lee-2098@live.co.uk>
2019-05-03 18:25:56 +0100

    black run through plus added to do list

--- tshark-tool.py
+++ tshark-tool.py
@@ -19,9 +19,9 @@ def capture_summary(x):
     """
     print("-" * 85)
     for packet in x:
-        #Get IP Src value from nested list
+        # Get IP Src value from nested list
         ip_src = packet["_source"]["layers"]["ip.src_host"][0]
-        #Get IP Dst value from nested list
+        # Get IP Dst value from nested list
         ip_dst = packet["_source"]["layers"]["ip.dst_host"][0]
 
         """
@@ -29,24 +29,24 @@ def capture_summary(x):
         This is a little convoluted but seems to work
         """
         for key in packet["_source"]["layers"].keys():
-            #Create 2 empty variables and a list
+            # Create 2 empty variables and a list
             port_type = ""
             dst_port = ""
             key_list = []
-            #Populate the list with all json dict keys
+            # Populate the list with all json dict keys
             key_list.append(key)
             if "tcp.dstport" in key_list:
-                #if term matches then set variable to tcp
+                # if term matches then set variable to tcp
                 port_type = "tcp"
-                #set variable to destination port depending on key
+                # set variable to destination port depending on key
                 dst_port = packet["_source"]["layers"]["tcp.dstport"][0]
             elif "udp.dstport" in key_list:
-                #if term matches then set variable to tcp
+                # if term matches then set variable to tcp
                 port_type = "udp"
-                #set variable to destination port depending on key
+                # set variable to destination port depending on key
                 dst_port = packet["_source"]["layers"]["udp.dstport"][0]
 
-        #Print formated table of results
+        # Print formated table of results
         print(
             " {:^20} | {:^20} | {:^20} | {:^20} ".format(
                 ip_src, ip_dst, port_type.upper(), dst_port
@@ -56,13 +56,12 @@ def capture_summary(x):
     print("")
 
 
-
-
-
 # ______________________________________________________________ #
 """
 Generator to loop through list and create a new src list
 """
+
+
 def src_createGenerator(x):
     src_list = []
     for src in x:
@@ -70,15 +69,11 @@ def src_createGenerator(x):
         yield src_list
 
 
-
 def dst_createGenerator(x):
     dst_list = []
     for dst in x:
         dst_list = dst["_source"]["layers"]["ip.dst_host"][0]
         yield dst_list
-
-
-
 
 
 # ______________________________________________________________ #
@@ -98,6 +93,7 @@ def most_frequent(x):
 top_dsts = {}
 top_srcs = {}
 
+
 def top_talkers():
     """
     Funtion to create two lists of top talkers (src & dst)
@@ -105,20 +101,19 @@ def top_talkers():
     msg1 = "IP:"
     msg2 = "Count:"
 
-    #get top 5 dsts using most_frequent function
-    #Store in dict with IP as key and count as value
-    #Insertion order will be top first so no need for OrdDict
+    # get top 5 dsts using most_frequent function
+    # Store in dict with IP as key and count as value
+    # Insertion order will be top first so no need for OrdDict
     for dst in most_frequent(new_dst_list):
         dst_ip = dst[0]
         count = dst[1]
         top_dsts[dst_ip] = count
 
-    #get top 5 Srcs using most_frequent function
+    # get top 5 Srcs using most_frequent function
     for src in most_frequent(new_src_list):
         src_ip = src[0]
         count = src[1]
         top_srcs[src_ip] = count
-
 
 
 def print_top_talkers():
@@ -136,7 +131,7 @@ def print_top_talkers():
     print("=" * 45)
     print("| {:^20} {:^20} |".format(src_msg, count_msg))
     for src_ip, src_count in top_srcs.items():
-            print("| {:^20} {:^20} |".format(src_ip, src_count))
+        print("| {:^20} {:^20} |".format(src_ip, src_count))
     print("=" * 45)
 
     print("\n")
@@ -145,28 +140,29 @@ def print_top_talkers():
     print("=" * 45)
     print("| {:^20} {:^20} |".format(dst_msg, count_msg))
     for dst_ip, dst_count in top_dsts.items():
-            print("| {:^20} {:^20} |".format(dst_ip, dst_count))
+        print("| {:^20} {:^20} |".format(dst_ip, dst_count))
     print("=" * 45)
+
 
 # ______________________________________________________________ #
 
 
-capture_summary(tshark_output) #Run capture summary function
+capture_summary(tshark_output)  # Run capture summary function
 
 mysrcgenerator = src_createGenerator(tshark_output)  # create a generator
-#Iterate over generator and store list
+# Iterate over generator and store list
 new_src_list = []
 for i in mysrcgenerator:
     new_src_list.append(i)
 
 
 mydstgenerator = dst_createGenerator(tshark_output)  # create a generator
-#Iterate over generator and store list
+# Iterate over generator and store list
 new_dst_list = []
 for i in mydstgenerator:
     new_dst_list.append(i)
 
-#Calling the top talkers function
+# Calling the top talkers function
 top_talkers()
 
 
@@ -191,6 +187,14 @@ sudo tshark -i any -n -c 100 -f "ip"  -T fields -e ip.src_host -e ip.dst_host -e
 
 ## Possible issues
 """
- * Total amount of occurences in top talkers is not equal to the amount of packets captured. I suspect this is due to the ip filter in the tshark command. L2 packets may count towards the capture count but be filtered out from the results and thus not stored/analysed by the script
+ 1)
+Total amount of occurences in top talkers is not equal to the amount of packets captured. 
+I suspect this is due to the ip filter in the tshark command. L2 packets may count towards the 
+capture count but be filtered out from the results and thus not stored/analysed by the script
 
+"""
+
+## To Do List
+"""
+1) Call tshark directly
 """
